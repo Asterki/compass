0ebd4df570ad434c48c665eb6d970374e38d2948
asterki <asterki.dev@proton.me>
2024-06-22 18:25:40 -0600

    Style: Some code style changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,107 +1,95 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
+import { useRouter } from "next/router";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+    const router = useRouter();
+
     return (
         <main
-            className={`flex min-h-screen flex-col items-center justify-between ${inter.className} text-slate-700 bg-purple-50`}
+            className={`flex min-h-screen flex-col items-center justify-between ${inter.className} bg-purple-50 text-slate-700`}
         >
-            <section className="min-h-screen flex flex-col items-center justify-between w-full">
-                <header className="p-4 w-full flex items-center justify-between md:px-24 px-2">
-                    <div className="md:w-auto w-full flex items-center md:justify-start justify-between md:gap-10 gap-2">
+            <section className="flex min-h-screen w-full flex-col items-center justify-between">
+                <header className="flex w-full items-center justify-between p-4 px-2 md:px-24">
+                    <div className="flex w-full items-center justify-between gap-2 md:w-auto md:justify-start md:gap-10">
                         <div>
-                            <h1 className="md:text-2xl text-lg font-bold">
-                                Class Compass
-                            </h1>
+                            <h1 className="text-lg font-bold md:text-2xl">Class Compass</h1>
                         </div>
                         <div>
-                            <ul className="flex justify-end md:gap-4 gap-2">
+                            <ul className="flex justify-end gap-2 md:gap-4">
                                 <li className="transition-all hover:scale-105">
-                                    <a
-                                        href="#"
-                                        className="text-purple-500 hover:underline font-semibold"
-                                    >
+                                    <a href="#" className="font-semibold text-purple-500 hover:underline">
                                         Home
                                     </a>
                                 </li>
                                 <li className="transition-all hover:scale-105">
-                                    <a
-                                        href="#"
-                                        className="text-purple-500 hover:underline font-semibold"
-                                    >
+                                    <a href="#" className="font-semibold text-purple-500 hover:underline">
                                         About
                                     </a>
                                 </li>
                                 <li className="transition-all hover:scale-105">
-                                    <a
-                                        href="#"
-                                        className="text-purple-500 hover:underline font-semibold"
-                                    >
+                                    <a href="#" className="font-semibold text-purple-500 hover:underline">
                                         Contact
                                     </a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="w-4/12 md:flex hidden items-center justify-center gap-6">
-                        <button className="rounded-md border-2 border-slate-200 p-2 w-1/2 font-bold transition-all hover:scale-105 shadow-md">
+                    <div className="hidden w-4/12 items-center justify-center gap-6 md:flex">
+                        <button
+                            className="w-1/2 rounded-md border-2 border-slate-200 p-2 font-bold shadow-md transition-all hover:scale-105"
+                            onClick={() => router.push("/login")}
+                        >
                             Login
                         </button>
-                        <button className="bg-gradient-to-br from-purple-400 to-purple-500 text-white p-2 rounded-md w-1/2 font-bold transition-all hover:scale-105 shadow-md">
+                        <button
+                            className="w-1/2 rounded-md bg-gradient-to-br from-purple-400 to-purple-500 p-2 font-bold text-white shadow-md transition-all hover:scale-105"
+                            onClick={() => router.push("/login")}
+                        >
                             Register
                         </button>
                     </div>
                 </header>
 
-                <section className="md:p-32 p-2 flex md:flex-row flex-col-reverse items-center justify-around gap-12 w-full">
-                    <div className="md:px-20 px-4 md:w-1/2 w-full text-center">
+                <section className="flex w-full flex-col-reverse items-center justify-around gap-12 p-2 md:flex-row md:p-32">
+                    <div className="w-full px-4 text-center md:w-1/2 md:px-20 md:text-left">
                         <h1 className="text-4xl font-bold">
-                            Manage your life in a single, centralized,
-                            easy-to-use platform Made for students, by students.
+                            Manage your life in a single, centralized, easy-to-use platform Made for students, by
+                            students.
                         </h1>
 
                         <br />
 
                         <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Sunt impedit dolore blanditiis odio ipsam
-                            sapiente voluptas in numquam sit quae expedita
-                            laudantium exercitationem eligendi et ullam, ratione
-                            neque eius odit, quasi ducimus architecto omnis
-                            ipsum. Debitis veritatis unde ut fugiat nemo nostrum
-                            vel ducimus, officia possimus esse labore harum
-                            quibusdam sit perferendis eius pariatur voluptatibus
-                            laborum molestias nobis, quae minus ea. Excepturi,
-                            dolor sapiente? Quibusdam soluta consectetur impedit
-                            ut aperiam sint maiores ipsum pariatur sed facere?
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt impedit dolore blanditiis
+                            odio ipsam sapiente voluptas in numquam sit quae expedita laudantium exercitationem eligendi
+                            et ullam, ratione neque eius odit, quasi ducimus architecto omnis ipsum. Debitis veritatis
+                            unde ut fugiat nemo nostrum vel ducimus, officia possimus esse labore harum quibusdam sit
+                            perferendis eius pariatur voluptatibus laborum molestias nobis, quae minus ea. Excepturi,
+                            dolor sapiente? Quibusdam soluta consectetur impedit ut aperiam sint maiores ipsum pariatur
+                            sed facere?
                         </p>
 
-                        <div className="flex md:justify-start justify-center gap-4">
-                            <button className="bg-gradient-to-br from-purple-400 to-purple-500 text-white font-semibold py-2 px-4 rounded-md mt-2 shadow-md transition-all hover:scale-105">
+                        <div className="flex justify-center gap-4 md:justify-start">
+                            <button className="mt-2 rounded-md bg-gradient-to-br from-purple-400 to-purple-500 px-4 py-2 font-semibold text-white shadow-md transition-all hover:scale-105">
                                 Register Now
                             </button>
-                            <button className=" font-semibold py-2 px-4 rounded-md mt-2 shadow-md border-2 border-slate-200 transition-all hover:scale-105">
+                            <button className="mt-2 rounded-md border-2 border-slate-200 px-4 py-2 font-semibold shadow-md transition-all hover:scale-105">
                                 See Features
                             </button>
                         </div>
                     </div>
-                    <div className="w-1/2 text-center flex items-center justify-center">
-                        <img
-                            src="https://placehold.co/500"
-                            alt="Example for now"
-                        />
+                    <div className="flex w-1/2 items-center justify-center text-center">
+                        <img src="https://placehold.co/500" alt="Example for now" />
                     </div>
                 </section>
 
-                <footer className="bg-gradient-to-br from-60% from-purple-500 to-pink-300  p-8 w-full flex items-center justify-center">
-                    <button className="bg-white p-2 rounded-md shadow-md">
-                        Register
-                    </button>
-                    <button className="bg-white p-2 rounded-md shadow-md">
-                        Login
-                    </button>
+                <footer className="flex w-full items-center justify-center bg-gradient-to-br from-purple-500 from-60% to-pink-300 p-8">
+                    <button className="rounded-md bg-white p-2 shadow-md">Register</button>
+                    <button className="rounded-md bg-white p-2 shadow-md">Login</button>
                 </footer>
             </section>
         </main>
