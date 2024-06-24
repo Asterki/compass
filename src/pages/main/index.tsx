8b29cc877e863cccefe6648600b92f117b2dad71
asterki <asterki.dev@proton.me>
2024-06-24 17:58:05 -0600

    Feat: Started the browse notes page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head"
 import { useRouter } from "next/router";
 
 import NavbarComponent from "@/components/navbar";
@@ -21,6 +22,10 @@ const MainPage = () => {
         <div
             className={`flex min-h-screen w-full flex-col items-center justify-between ${inter.className} bg-purple-50 text-slate-700 dark:bg-slate-900 dark:text-slate-300`}
         >
+            <Head>
+                <title>Dashboard | ClassCompass</title>
+            </Head>
+
             {status == "loading" && "Loading..."}
 
             {status == "authenticated" &&  session.user !== undefined && (
