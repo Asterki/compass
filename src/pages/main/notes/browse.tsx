7d4b220e75daaa2ab0e91b663f870935b8bdcde0
asterki <asterki.dev@proton.me>
2024-06-25 20:36:31 -0600

    Notes browser

--- src/pages/main/notes/browse.tsx
+++ src/pages/main/notes/browse.tsx
@@ -3,9 +3,16 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
+
+// Components
 import NavbarComponent from "@/components/navbar";
+import * as Collapsible from "@radix-ui/react-collapsible";
 
 import { Inter } from "next/font/google";
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons/faChevronCircleDown";
 const inter = Inter({ subsets: ["latin"] });
 
 const NotesBrowsePage = () => {
@@ -31,7 +38,50 @@ const NotesBrowsePage = () => {
                 <main className="flex w-full flex-col items-center justify-between">
                     <NavbarComponent session={session} />
 
-                    <h1>Notes Browse Page</h1>
+                    {/* Browser  */}
+                    <div className="w-9/12">
+                        <h1 className="text-2xl">Browse Notes</h1>
+
+                        <Collapsible.Root className="group w-full mt-2">
+                            <Collapsible.Trigger className="group mt-2 flex w-full cursor-pointer items-center justify-between rounded-md bg-slate-800 p-2">
+                                <div>
+                                    <FontAwesomeIcon icon={faFolder} className="mr-2 fill-white" />
+                                    Notes Folder 1
+                                </div>
+                                <FontAwesomeIcon
+                                    icon={faChevronCircleDown}
+                                    className="fill-white transition-all group-data-[state=open]:rotate-180"
+                                />
+                            </Collapsible.Trigger>
+                            <Collapsible.Content className="-my-2 flex items-stretch">
+                                <div className="w-full rounded-md border-2 border-slate-800 p-2">
+                                    <div className="my-2 flex w-full cursor-pointer items-center justify-between rounded-md bg-slate-800 p-2">
+                                        <div>
+                                            <FontAwesomeIcon icon={faFolder} className="mr-2 fill-white" />
+                                            Notes Folder 1
+                                        </div>
+                                        <FontAwesomeIcon icon={faChevronCircleDown} className="fill-white" />
+                                    </div>
+                                    <div className="my-2 flex w-full cursor-pointer items-center justify-between rounded-md bg-slate-800 p-2">
+                                        <div>
+                                            <FontAwesomeIcon icon={faFolder} className="mr-2 fill-white" />
+                                            Notes Folder 1
+                                        </div>
+                                        <FontAwesomeIcon icon={faChevronCircleDown} className="fill-white" />
+                                    </div>
+                                    <div className="my-2 flex w-full cursor-pointer items-center justify-between rounded-md bg-slate-800 p-2">
+                                        <div>
+                                            <FontAwesomeIcon icon={faFolder} className="mr-2 fill-white" />
+                                            Notes Folder 1
+                                        </div>
+                                        <FontAwesomeIcon icon={faChevronCircleDown} className="fill-white" />
+                                    </div>
+                                </div>
+                            </Collapsible.Content>
+                        </Collapsible.Root>
+
+                        
+                    </div>
                 </main>
             )}
         </div>
