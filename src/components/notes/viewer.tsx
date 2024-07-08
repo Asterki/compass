3e37b41feb192cce84164141a3863b265823acb5
asterki <asterki.dev@proton.me>
2024-07-07 19:55:29 -0600

    Feat: Added some styles to the markdown previewer

--- src/components/notes/viewer.tsx
+++ src/components/notes/viewer.tsx
@@ -1,5 +1,7 @@
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import remarkLintListItemIndent from 'remark-lint-list-item-bullet-indent'
+import remarkBreaks from 'remark-breaks'
 
 interface Note {
     note: {
@@ -25,7 +27,9 @@ interface Note {
 const NoteViewer: React.FC<Note> = props => {
     return (
         <Markdown
-            remarkPlugins={[remarkGfm]}
+            remarkPlugins={[remarkGfm, remarkBreaks]}
+            className={`flex w-9/12 flex-col gap-2`}
+            skipHtml={true}
             components={{
                 h1: ({ node, ...props }) => <h1 className="text-2xl font-bold" {...props} />,
                 h2: ({ node, ...props }) => <h2 className="text-xl font-bold" {...props} />,
@@ -33,20 +37,22 @@ const NoteViewer: React.FC<Note> = props => {
                 h4: ({ node, ...props }) => <h4 className="text-base font-bold" {...props} />,
                 h5: ({ node, ...props }) => <h5 className="text-sm font-bold" {...props} />,
                 h6: ({ node, ...props }) => <h6 className="text-xs font-bold" {...props} />,
-                p: ({ node, ...props }) => <p className="text-base" {...props} />,
-                a: ({ node, ...props }) => <a className="text-blue-500" {...props} />,
-                ul: ({ node, ...props }) => <ul className="list-disc" {...props} />,
-                ol: ({ node, ...props }) => <ol className="list-decimal" {...props} />,
-                li: ({ node, ...props }) => <li className="text-base" {...props} />,
-                table: ({ node, ...props }) => <table className="table-auto" {...props} />,
-                thead: ({ node, ...props }) => <thead className="bg-gray-50" {...props} />,
-                tbody: ({ node, ...props }) => <tbody className="bg-white" {...props} />,
-                th: ({ node, ...props }) => <th className="border border-gray-200 p-2" {...props} />,
-                td: ({ node, ...props }) => <td className="border border-gray-200 p-2" {...props} />,
-                blockquote: ({ node, ...props }) => (
-                    <blockquote className="border-l-4 border-gray-200 p-2" {...props} />
+                a: ({ node, ...props }) => <a className="text-blue-500 hover:underline" {...props} />,
+                table: ({ node, ...props }) => (
+                    <table border={0} className="table-auto rounded-md" {...props} />
                 ),
-                code: ({ node, ...props }) => <code className="bg-gray-50 p-1" {...props} />,
+                thead: ({ node, ...props }) => <thead className="!bg-slate-950" {...props} />,
+                tbody: ({ node, ...props }) => <tbody className="!bg-slate-800" {...props} />,
+                th: ({ node, ...props }) => <th className="border border-gray-700 p-2" {...props} />,
+                td: ({ node, ...props }) => <td className="border border-gray-700 p-2" {...props} />,
+                blockquote: ({ node, ...props }) => (
+                    <blockquote className="block border-l-2 border-gray-200/20 p-2 *:block" {...props} />
+                ),
+                p: ({ node, ...props }) => <p className="w-full text-base" {...props} />,
+                ul: ({ node, ...props }) => <ul className="list-disc list-inside" {...props} />,
+                ol: ({ node, ...props }) => <ol className="list-decimal list-inside" {...props} />,
+                li: ({ node, ...props }) => <li className="text-base" {...props} />,
+                code: ({ node, ...props }) => <code className="rounded-md bg-slate-700 p-1" {...props} />,
                 pre: ({ node, ...props }) => <pre className="bg-gray-50 p-2" {...props} />,
                 img: ({ node, ...props }) => <img className="max-w-full" {...props} />,
                 hr: ({ node, ...props }) => <hr className="border-t border-gray-200" {...props} />,
@@ -55,7 +61,7 @@ const NoteViewer: React.FC<Note> = props => {
                 del: ({ node, ...props }) => <del className="line-through" {...props} />,
                 sup: ({ node, ...props }) => <sup className="text-xs" {...props} />,
                 sub: ({ node, ...props }) => <sub className="text-xs" {...props} />,
-                br: ({ node, ...props }) => <br {...props} />,
+                br: ({ node, ...props }) => <br {...props} />
             }}
         >
             {props.note.content}
