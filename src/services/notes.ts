31a7204352b0faccf5e21b8618266abdfbdd1edc
asterki <asterki.dev@proton.me>
2024-07-08 18:10:28 -0600

    Feat: Basic note service

--- src/services/notes.ts
+++ src/services/notes.ts
@@ -31,12 +31,7 @@ interface Note {
     links: Link[]
 }
 
-const createNote = async (
-    title: string,
-    content: string,
-    folderId: string,
-    userId: string,
-) => {
+const createNote = async (title: string, content: string, folderId: string, userId: string) => {
     const noteID = uuidv4()
 
     const note: Note = {
@@ -63,3 +58,45 @@ const createNote = async (
 
     return note.id
 }
+
+const deleteNote = (noteId: string) => {
+    prismaClient.note.delete({
+        where: {
+            id: noteId
+        }
+    })
+}
+
+const getNote = async (noteId: string) => {
+    const note = await prismaClient.note.findUnique({
+        where: {
+            id: noteId
+        }
+    })
+
+    return note
+}
+
+const getNotes = async (folderId: string) => {
+    const notes = await prismaClient.note.findMany({
+        where: {
+            folder_id: folderId
+        }
+    })
+
+    return notes
+}
+
+const updateNote = async (noteId: string, title: string, content: string) => {
+    await prismaClient.note.update({
+        where: {
+            id: noteId
+        },
+        data: {
+            title,
+            content
+        }
+    })
+}
+
+export { createNote, deleteNote, getNote, getNotes, updateNote }
