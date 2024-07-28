20c2cbc8c098003bb77ea10b84c8f64622673cd4
asterki <asterki.dev@proton.me>
2024-07-27 19:54:20 -0600

    Feat: New panel page layout

--- src/pages/panel/index.tsx
+++ src/pages/panel/index.tsx
@@ -5,7 +5,24 @@ import NavbarComponent from '@/components/layout/navbar'
 import Button from '@/components/ui/button'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFolder, faNoteSticky, faStickyNote } from '@fortawesome/free-solid-svg-icons'
+import {
+    faBell,
+    faBook,
+    faCalendar,
+    faCalendarAlt,
+    faClock,
+    faEgg,
+    faFolder,
+    faGraduationCap,
+    faHourglass2,
+    faMoneyBill1Wave,
+    faNoteSticky,
+    faSchool,
+    faShip,
+    faStickyNote,
+    faTasks,
+    faTimesRectangle
+} from '@fortawesome/free-solid-svg-icons'
 
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
@@ -41,15 +58,106 @@ const MainPage = () => {
                 <main className="flex w-full flex-col items-center justify-between">
                     <NavbarComponent session={session} />
 
-                    <section className="p-4">
+                    <section className="flex flex-col flex-wrap md:items-stretch items-center justify-center gap-4 p-4 md:flex-row">
                         <Link
                             href={'/panel/notes/browse'}
-                            className="flex cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-white transition-all hover:scale-105"
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
                         >
                             <FontAwesomeIcon icon={faStickyNote} className="fill-white text-8xl" />
                             <h1 className="text-xl font-bold">Notes</h1>
+                            <p className="text-white/80">Access your notes and create new ones.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/calendar'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faCalendarAlt} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Calendar</h1>
+                            <p className="text-white/80">View your calendar and schedule events.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/tasks'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faTasks} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Tasks</h1>
+                            <p className="text-white/80">Manage your tasks and to-do lists.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/recipes'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faEgg} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Recipes</h1>
+                            <p className="text-white/80">Access your cooking recipes and create new ones.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/school'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faGraduationCap} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">School Dashboard</h1>
+                            <p className="text-white/80">Access your school dashboard and manage your courses.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/reminders'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faBell} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Reminders</h1>
+                            <p className="text-white/80">Access your reminders and create new ones.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/pomodoro'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faClock} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Pomodoro</h1>
+                            <p className="text-white/80">Access your pomodoro timer and manage your sessions.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/goals'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faShip} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Goal Setting</h1>
+                            <p className="text-white/80">Access your goal setting dashboard and manage your goals.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/books'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faBook} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Book Manager</h1>
+                            <p className="text-white/80">Access your book manager and manage your reading list.</p>
+                        </Link>
+
+                        <Link
+                            href={'/panel/expenses'}
+                            className="flex md:w-3/12 w-11/12 cursor-pointer select-none flex-col items-center justify-center rounded-md bg-white/10 p-4 text-center text-white transition-all hover:scale-105"
+                        >
+                            <FontAwesomeIcon icon={faMoneyBill1Wave} className="fill-white text-8xl" />
+                            <h1 className="text-xl font-bold">Expense Record</h1>
+                            <p className="text-white/80">Access your expense record and manage your finances.</p>
                         </Link>
                     </section>
+
+                    <footer className="flex w-full items-center justify-center p-4">
+                        <Button
+                            onClick={() => router.push('/panel/settings')}
+                            variant='secondary'
+                        >
+                            Manage Sections
+                        </Button>
+                    </footer>
                 </main>
             )}
         </div>
